5bfc4d5bf8a935f4efcc1afc395269d1b948860e
utkarsharchit <52402882+utkarsharchit@users.noreply.github.com>
2019-07-04 02:14:18 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,27 +1,30 @@
-import socket
-import sys
 
-host=''
-port=5555
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-try:
-s.bind((host,port))
-except socket.error as e:
-print(str(e))
-s.listen(5)\conn,addr=s.accept()
-print('Waiting for a connection.')
-def threaded_client(conn):
-conn.send(str.encode('Welcome')
+import socket                
+  
 
-while True:
-data=conn.recv(2048)
-reply='Server output: '+data .decode('utf-8')
-if not data:
-break
-conn.sendall(str.encode(reply))
-conn.close()
-while True:
-conn,addr=s.accept()
-print('connected to:+addr[0]+':'+str9addr[1])
-start_new_thread(threaded_client,(conn,))
+s = socket.socket()          
+print "Socket successfully created"
+  
+
+port = 12345                
+  
+s.bind(('', port))         
+print "socket binded to %s" %(port) 
+  
+
+s.listen(5)      
+print "socket is listening"            
+
+while True: 
+  
+    
+   c, addr = s.accept()      
+   print 'Got connection from', addr 
+  
+     
+   c.send('Thank you for connecting') 
+  
+    
+   c.close() 
+
